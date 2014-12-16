178ebd205f495c5a69e9b486e795ecfcc99245d0
Charanjit Singh <ckhabra007@gmail.com>
2014-12-16 19:24:02 +0530

    Start workign on billing wizard

--- client/html/js/billing_wizard.js
+++ client/html/js/billing_wizard.js
@@ -6,16 +6,25 @@ Template.billing_wizard.rendered = function() {
 
 Template.billing_wizard.events({
   "click .step-1": function() {
-    Session.set("signup_steps_done", 1);
+    Session.set("billing_steps_done", 1);
   },
   "click .step-2": function() {
-    Session.set("signup_steps_done", 2);
+    Session.set("billing_steps_done", 2);
   },
   "click .step-3": function() {
-    Session.set("signup_steps_done", 3);
+    Session.set("billing_steps_done", 3);
   },
   "click .step-4": function() {
-    Session.set("signup_steps_done", 4);
+    Session.set("billing_steps_done", 4);
+  }
+});
+
+Template.billing_wizard.helpers({
+  "can_go_previous": function() {
+    return Session.get("billing_steps_done") > 1;
+  },
+  "can_go_forward": function() {
+    return Session.get("billing_steps_done") < 4;
   }
 });
 
@@ -23,10 +32,10 @@ Template.billing_wizard.events({
  * Autorun to update the progress bar below the signup form
  */
 Tracker.autorun(function() {
-  var signup_steps_done = Session.get("signup_steps_done"),
+  var billing_steps_done = Session.get("billing_steps_done"),
       $progress_bar = $(".progress-bar-inverse");
 
-  switch(signup_steps_done) {
+  switch(billing_steps_done) {
   case 1:
     $progress_bar.width("25%");
     break;
